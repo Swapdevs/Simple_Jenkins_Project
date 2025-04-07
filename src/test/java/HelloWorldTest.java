1e3783f858470c9023edc5d943eb5298acbcbd11
Christopher Akinsanmi <christopher.akinsanmi@gmail.com>
2025-04-07 12:12:41 +0100

    initial commit

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void testGreeting() {
+        HelloWorld hw = new HelloWorld();
+        assertEquals("Hello, World!", hw.getGreeting());
+    }
+}
